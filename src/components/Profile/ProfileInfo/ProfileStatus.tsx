5a78690cde934a6bcef4f19f32e41f934ccd3467
msseleznev <mikestm8979@gmail.com>
2022-04-12 15:10:16 +0400

    lesson 74 - componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -30,6 +30,14 @@ class ProfileStatus extends React.Component<ProfileStatusType> {
         })
     }
 
+    componentDidUpdate(prevProps: Readonly<ProfileStatusType>, prevState: Readonly<{}>) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     render() {
         return (
             <div>
